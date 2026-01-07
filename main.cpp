55b071dd374a3e9a3c04794ff246371f4ae3f8dc
garysingh08 <gjagpal8@yahoo.com>
2026-01-07 13:26:45 -0800

    Update main.cpp
    Added function to add numbers, fixed a syntax code when using namespace std;

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include<iostream>
-#using namespace std;
+using namespace std;
+
+int addNums(int x, int y){
+  return x+y;
+}  
 
 int main(){
 
   cout << "Hi ya" << endl;
+  cout << addNums(2,3) << endl;
 
   return 0;
 }
