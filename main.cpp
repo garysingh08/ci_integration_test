5221c3e67adf305070f229e31c4fc2272b995dd6
garysingh08 <gjagpal8@yahoo.com>
2026-01-10 12:27:57 -0800

    Moved unit tests functions
    Removed the addNums function and its associated tests into math_utils.h file

--- main.cpp
+++ main.cpp
@@ -1,25 +1,6 @@
 #include <iostream>
 #include <cassert>
-
-// The function we are testing
-int addNums(int x, int y) {
-    return x + y;
-}
-
-void test_positive_numbers() {
-    assert(addNums(5, 5) == 11);
-    std::cout << "✅ Test Passed: 5 + 5 = 10" << std::endl;
-}
-
-void test_negative_numbers() {
-    assert(addNums(-1, -1) == -2);
-    std::cout << "✅ Test Passed: -1 + -1 = -2" << std::endl;
-}
-
-void test_zero() {
-    assert(addNums(0, 100) == 100);
-    std::cout << "✅ Test Passed: 0 + 100 = 100" << std::endl;
-}
+#include "math_utils.h" // file that holds math functions
 
 int main() {
     std::cout << "Running Automated Tests..." << std::endl;
